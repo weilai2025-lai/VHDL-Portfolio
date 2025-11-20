4987fc3bf21a10a167a5a6dba0b951e32a2e1545
WEI-IN LAI <weilai2025@u.northwestern.edu>
2025-11-19 21:51:12 -0600

    update again

--- miniVGA/vga_dynamic_tb_pipe.vhd
+++ miniVGA/vga_dynamic_tb_pipe.vhd
@@ -5,15 +5,15 @@ use IEEE.numeric_std.all;
 use std.textio.all;
 use ieee.std_logic_textio.all;
 
-entity vga_dynamic_tb is
-end entity vga_dynamic_tb;
+entity vga_dynamic_tb_pipe is
+end entity vga_dynamic_tb_pipe;
 
-architecture sim of vga_dynamic_tb is
+architecture sim of vga_dynamic_tb_pipe is
   ------------------------------------------------------------------
   -- 1) 基本時脈 / reset
   ------------------------------------------------------------------
   constant T_50         : time   := 20 ns;   -- 50 MHz
-  constant TOTAL_FRAMES : integer := 10;     -- 要截多少偵
+  constant TOTAL_FRAMES : integer := 60;     -- 要截多少偵
 
   signal clock_50   : std_logic := '0';
   signal reset_n    : std_logic := '0';
@@ -33,7 +33,10 @@ architecture sim of vga_dynamic_tb is
   signal vga_red      : std_logic_vector(9 downto 0);
   signal vga_green    : std_logic_vector(9 downto 0);
   signal vga_blue     : std_logic_vector(9 downto 0);
-
+  ----pipeline Alignment訊號-----------------------------
+  signal video_on_pipe : std_logic;
+  signal pixel_row_pipe : std_logic_vector(9 downto 0);
+  signal pixel_column_pipe :std_logic_vector(9 downto 0);
   ------------------------------------------------------------------
   -- 3) tank 相關訊號
   ------------------------------------------------------------------
@@ -58,6 +61,13 @@ architecture sim of vga_dynamic_tb is
   signal bottom_bullet_y      : std_logic_vector(9 downto 0);
   signal fire_bottom          : std_logic := '0';
   signal bottom_tank_dir      : std_logic;
+  
+  -- top bullet 訊號
+  ------------------------------------------------------------------
+  signal top_bullet_active : std_logic;
+  signal top_bullet_x      : std_logic_vector(9 downto 0);
+  signal top_bullet_y      : std_logic_vector(9 downto 0);
+  signal top_tank_dir      : std_logic;
 
 begin
   ------------------------------------------------------------------
@@ -85,16 +95,19 @@ begin
   ------------------------------------------------------------------
   -- 5) 實例化 vga_sync
   ------------------------------------------------------------------
-  u_sync : entity work.vga_sync
+  u_sync : entity work.vga_sync_pipe
     port map(
       clock_50mhz    => clock_50,
       horiz_sync_out => horiz_sync,
       verti_syn_out  => verti_sync,
       video_on       => video_on,
+		video_on_pipe  => video_on_pipe,
       pixel_clock    => vga_clk,
       eof            => eof,
       pixel_row      => pixel_row,
-      pixel_column   => pixel_column
+		pixel_row_pipe => pixel_row_pipe,
+      pixel_column   => pixel_column,
+		pixel_column_pipe   => pixel_column_pipe
     );
 
   ------------------------------------------------------------------
@@ -140,13 +153,13 @@ begin
       freeze     => freeze,
       tankx      => top_tank_x,
       tanky      => top_tank_y,
-      tank_dir   => open
+      tank_dir   => top_tank_dir
     );
 
   ------------------------------------------------------------------
   -- 6c) 實例化 bottom bullet（只給 bottom tank 用）
   ------------------------------------------------------------------
-  u_bullet_bottom : entity work.tank_bullet_bottom
+  u_bullet_bottom : entity work.tank_bullet_bottom_refine
     generic map(
       SCREEN_WIDTH  => 640,
       SCREEN_HEIGHT => 480,
@@ -170,6 +183,32 @@ begin
       bullet_y      => bottom_bullet_y
     );
 
+  -- 6c) 實例化 top bullet（只給 top tank 用）
+  ------------------------------------------------------------------
+  u_bullet_top : entity work.tank_bullet_top_refine
+    generic map(
+      SCREEN_WIDTH  => 640,
+      SCREEN_HEIGHT => 480,
+      TANK_W        => 32,
+      TANK_H        => 10,
+      BULLET_SIZE   => 5,
+      DIR_UP        => '0',   -- 往下飛
+      STEP_PIXELS   => 20
+    )
+    port map(
+      clk           => vga_clk,
+      rst_n         => reset_n,
+      move_pulse    => move_pulse,
+      fire          => fire_bottom,  -- 下面 dump_proc 只會拉一次
+      tankx         => top_tank_x,
+      tanky         => top_tank_y,
+		tank_dir      => top_tank_dir,
+		speed_sel     => speed_sel,
+      bullet_active => top_bullet_active,
+      bullet_x      => top_bullet_x,
+      bullet_y      => top_bullet_y
+    );
+
   ------------------------------------------------------------------
   -- 7) 實例化 pixelGenerator_dynamicbulletmodule
   ------------------------------------------------------------------
@@ -189,6 +228,9 @@ begin
       bottom_bullet_active => bottom_bullet_active,
       bottom_bullet_x      => bottom_bullet_x,
       bottom_bullet_y      => bottom_bullet_y,
+      top_bullet_active    => top_bullet_active,
+      top_bullet_x         => top_bullet_x,
+      top_bullet_y         => top_bullet_y,		
       red_out              => vga_red,
       blue_out             => vga_blue,
       green_out            => vga_green
@@ -200,7 +242,7 @@ begin
   --    fire_bottom 只在「第 1 frame 開頭」拉一次（只發射一顆子彈）
   ------------------------------------------------------------------
   dump_proc : process
-    file f          : text open write_mode is "frames_dump1.txt";
+    file f          : text open write_mode is "frames_dump111.txt";
     variable L      : line;
     variable row_i  : integer;
     variable col_i  : integer;
@@ -240,10 +282,10 @@ begin
       prev_eof := eof;
 
       -- 只在 video_on = 1 的時候記錄畫面
-      if (video_on = '1') and (frame_idx > 0) then
+      if (video_on_pipe = '1') and (frame_idx > 0) then
         -- 把座標轉成整數
-        row_i := to_integer(unsigned(pixel_row));
-        col_i := to_integer(unsigned(pixel_column));
+        row_i := to_integer(unsigned(pixel_row_pipe));
+        col_i := to_integer(unsigned(pixel_column_pipe));
         -- 把顏色強度轉成整數
         r_i   := to_integer(unsigned(vga_red));
         g_i   := to_integer(unsigned(vga_green));
